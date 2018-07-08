b26ff49514a5d40b4fbd7788a97367aeb87f5045
try-to-flow <40962529+try-to-flow@users.noreply.github.com>
2018-07-08 21:15:33 +0300

    first pull request: learnpython (#1)
    Yea, motherfucker!

---
+++ learnpython/lesson1/variables.py
@@ -0,0 +1,22 @@
+variable1 = 3
+variable2 = 5
+variable3 = variable1 + variable2
+print(variable1)
+print(variable2)
+print(variable3)
+
+variable1 = 2
+variable1 += 3
+print(variable1)
+variable1 -= 3
+print(variable1)
+variable1 *= 3
+print(variable1)
+variable1 /= 3
+print(variable1)
+variable1 **= 3
+print(variable1)
+variable1 //= 3
+print(variable1)
+variable1 %= 3
+print(variable1)
